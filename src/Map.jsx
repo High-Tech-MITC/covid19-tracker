5f150409ac09b9c365645019a5f027103a285b81
faruqismael <farusmith5@gmail.com>
2021-03-20 20:02:07 +0300

    removed unused variable

--- src/Map.jsx
+++ src/Map.jsx
@@ -10,27 +10,25 @@ const Map = ({ countries, country, center, zoom }) => {
                 <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-                {43.45 === center[0] && 35.3 === center[1] ? console.log("nothing") :
-                    <CircleMarker center={center} color="green" radius={20}>
-                        <Popup>
-                            <div className="map__popup">
-                                {country.name} <br />
-                                {country.cases} <br />
-                                {country.deaths} <br />
-                            </div>
-                        </Popup>
-                    </CircleMarker>
-                }
+                <CircleMarker center={center} color="green" radius={20}>
+                    <Popup>
+                        <div className="map__popup">
+                            {country.name} <br />
+                            {country.cases} <br />
+                            {country.deaths} <br />
+                        </div>
+                    </Popup>
+                </CircleMarker>
+
 
                 {countries.map(country => {
-                    console.log(country);
                     const reduceSquare = (value) => {
                         return (
                             Math.floor(Math.sqrt(value / 1000))
                         )
                     }
                     return (
-                        <CircleMarker radius={reduceSquare(country.cases)} color="red" center={country.center}>
+                        <CircleMarker radius={reduceSquare(country.cases)} color="black" center={country.center}>
                             <Popup>
                                 <div className="map__popup">
                                     <h2 className="black">{country.name}</h2>
@@ -44,7 +42,6 @@ const Map = ({ countries, country, center, zoom }) => {
 
                 })}
 
-                {console.log(countries[9])}
             </LeafletMap>
         </div>
     );
